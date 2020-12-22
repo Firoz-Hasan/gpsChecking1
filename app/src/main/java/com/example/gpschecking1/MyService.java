1fc80aa1274646d7fe98ac745e200b5041fc9d8f
firoz <diganto007@gmail.com>
2020-12-22 17:56:03 +0600

    foreground service added

--- app/src/main/java/com/example/gpschecking1/MyService.java
+++ app/src/main/java/com/example/gpschecking1/MyService.java
@@ -1,18 +1,59 @@
 package com.example.gpschecking1;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
     public MyService() {
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "channel_biz_motion_location";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Location Notification Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle(getString(R.string.app_name))
+                    .setContentText("Running in Background")
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentIntent(pendingIntent)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+
+            Notification notification = builder.build();
+            startForeground(1, notification);
+        }
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
         onTaskRemoved(intent);
         Toast.makeText(getApplicationContext(),"This is a Service running in Background",
                 Toast.LENGTH_SHORT).show();
+        Log.d("hhh", "hhhhhhhhhhhhhhhhhhhhhhhhhh");
         return START_STICKY;
     }
     @Override
